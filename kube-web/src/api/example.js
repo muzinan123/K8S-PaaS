86591fac32201efc90a8003c78fe35589aed267e
James Li <muzinan123@users.noreply.github.com>
2024-09-25 13:22:56 +0800

    Delete kube-web directory

--- kube-web/src/api/example.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function examplePing() {
-  return request({
-    url: '/example/ping',
-    method: 'get'
-  })
-}
--- kube-web/src/api/namespace.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function getNamespaceList() {
-  return request({
-    url: '/k8s/namespace',
-    method: 'get'
-  })
-}
